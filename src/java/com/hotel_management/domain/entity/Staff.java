2dec8bea87bfb0fb677475a0f58c1441f1528e4b
nguyenduongthuan75321@gmail.com <nguyenduongthuan75321@gmail.com>
2025-09-28 16:47:57 +0700

    feat: setting and testing pool connection

---
+++ src/java/com/hotel_management/domain/entity/Staff.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hotel_management.domain.entity;
+
+/**
+ *
+ * @author DELL
+ */
+public class Staff {
+    private int staffid;
+    private String fullname;
+    private String role;
+    private String username;
+    private String password;
+    private String phone;
+    private String email;
+
+    public Staff() {
+        staffid=0;
+        fullname="";
+        //todo
+    }
+
+    public Staff(String fullname, String role, String username, String password, String phone, String email) {
+        this.fullname = fullname;
+        this.role = role;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Staff(int staffid, String fullname, String role, String username, String password, String phone, String email) {
+        this.staffid = staffid;
+        this.fullname = fullname;
+        this.role = role;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getStaffid() {
+        return staffid;
+    }
+
+    public void setStaffid(int staffid) {
+        this.staffid = staffid;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
